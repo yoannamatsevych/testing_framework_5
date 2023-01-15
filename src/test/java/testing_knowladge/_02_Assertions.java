e5d22609c37cd485da42dd33d5f042b400854b03
Yoanna Matsevych <yana.matsevych@gmail.com>
2023-01-15 13:48:11 -0600

    Assertion

---
+++ src/test/java/testing_knowladge/_02_Assertions.java
@@ -0,0 +1,42 @@
+package testing_knowladge;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _02_Assertions {
+
+    @Test
+    public void test1(){
+        Assert.assertTrue(true);
+        Assert.assertTrue(1 == 1);
+        Assert.assertTrue(65 > 45);
+        Assert.assertTrue(10 <= 10);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertFalse(false);
+        Assert.assertFalse(3 > 5);
+    }
+
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("hello", "hello");
+        Assert.assertNotEquals("Hello", "hello");
+    }
+
+
+    @Test
+    public void assertionMassages(){
+        Assert.assertTrue(true, "This validation FAILED");
+        Assert.assertEquals("Hello", "hello", "This two text does not equals");
+
+    }
+
+    @Test
+    public void testFailed(){
+        Assert.fail("This test fails due to bug 123ABC");
+    }
+
+}
