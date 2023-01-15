1b16c7f75882901f9c137f07cd4a038c8371b87d
Yoanna Matsevych <yana.matsevych@gmail.com>
2023-01-15 14:29:04 -0600

    priority

---
+++ src/test/java/testing_knowladge/_03_Priority_andDependency.java
@@ -0,0 +1,30 @@
+package testing_knowladge;
+
+import org.testng.annotations.Test;
+
+public class _03_Priority_andDependency {
+
+    @Test(priority = 1)
+    public void login(){
+        System.out.println("This is login");
+    }
+
+    @Test(dependsOnMethods = "login")
+    public void logout(){
+        System.out.println("This is logout");
+    }
+
+    @Test(dependsOnMethods = {"login", "logout"})
+    public void forgotPassword(){
+        System.out.println("This is forgot password");
+    }
+
+    @Test(dependsOnMethods = {"login", "logout", "forgotPassword"})
+    public void invalidLogin(){
+        System.out.println("This is invalid login");
+    }
+
+
+
+
+}
---
+++ src/test/java/testing_knowladge/_04_TestDescription.java
@@ -0,0 +1,20 @@
+package testing_knowladge;
+
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+public class _04_TestDescription {
+/*
+    @Test(priority = 1, description = "Login with valid credentials")
+    public void login(){
+        System.out.println("This is login");
+    }
+
+
+    @Ignore
+    @Test(priority = 2, description = "Login with invalid credentials")
+    public void invalidLogin(){
+        System.out.println("This is invalid login");
+    }
+*/
+}
